1edbad1738495b8262c6c3c62ac791dd0797b43b
brmyfun <brmyfun@163.com>
2020-11-03 22:39:46 +0800

    升级vue和ant-design-vue版本

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <!-- <router-view /> -->
-  <a-config-provider :locale="locale">
+  <router-view />
+  <!-- <a-config-provider :locale="locale">
     <router-view />
-  </a-config-provider>
+  </a-config-provider> -->
 </template>
 <script>
-import locale from "ant-design-vue/es/locale/zh_CN";
+// import locale from "ant-design-vue/es/locale/zh_CN";
+// import moment from "moment";
+// import "moment/locale/zh-cn";
+// moment.locale("zh-cn");
 export default {
   name: "App",
   data() {
     return {
-      locale,
+      // locale,
+      // moment,
     };
   },
 };
--- src/components/MarketCalendar.vue
+++ src/components/MarketCalendar.vue
@@ -8,25 +8,35 @@
           marginLeft: '12px',
         }"
       >
-        <a-calendar
+        <a-config-provider :locale="locale">
+          <a-calendar
+            v-model:value="value"
+            :fullscreen="false"
+            @panelChange="onPanelChange"
+          />
+        </a-config-provider>
+        <!-- <a-calendar
           v-model:value="value"
           :fullscreen="false"
           @panelChange="onPanelChange"
-        />
+        /> -->
       </div>
     </a-col>
     <a-col :span="12"></a-col>
   </a-row>
 </template>
 <script>
+import locale from "ant-design-vue/es/locale/zh_CN";
 import moment from "moment";
 // import "moment/locale/zh-cn";
+// moment.locale("zh-cn");
 export default {
   name: "MarketCalendar",
   data() {
     return {
-      value: moment("2017-01-25"),
+      locale,
       moment,
+      value: "",
     };
   },
   methods: {
